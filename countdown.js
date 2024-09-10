9411fe8ae735832654a6e334cce6e3468b192237
santiavelas <velazsant2@gmail.com>
2024-09-10 12:43:09 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuenta', {
 	year: 2024, // required
 	month: 9, // required
 	day: 10, // required
-	hours: 19, // Default is 0 [0-23] integer
+	hours: 20, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
